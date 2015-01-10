6c039f0f2fe7f8d2bbef4e4120115978a4aa8da0
EK <ek@neomerx.com>
2015-01-11 02:25:21 +0300

    Fix scrutinizer issues

---
+++ src/Commands/GenerateImageCommand.php
@@ -0,0 +1,34 @@
+<?php namespace Neomerx\Core\Commands;
+
+use \Neomerx\Core\Support as S;
+use \Illuminate\Queue\Jobs\Job;
+use \Neomerx\Core\Models\ImagePath;
+use \Illuminate\Support\Facades\Log;
+use \Neomerx\Core\Models\ImageFormat;
+
+class GenerateImageCommand
+{
+    /**
+     * @param Job   $job
+     * @param array $data
+     */
+    public function byFormat(Job $job, array $data)
+    {
+        $formatName = S\array_get_value($data, 'name');
+
+        if ($formatName !== null) {
+            /** @var ImageFormat $imageFormatModel */
+            $imageFormatModel = new ImageFormat();
+            /** @var ImagePath $path */
+            $imageFormatModel = $imageFormatModel->selectByName($formatName)->firstOrFail();
+
+            foreach ($imageFormatModel->paths as $path) {
+                $imageUpdated = $path->generateImage();
+                /** @noinspection PhpUndefinedMethodInspection */
+                $imageUpdated ?: Log::error(trans('nm::errors.image_generation_failed'));
+            }
+        }
+
+        $job->delete();
+    }
+}
