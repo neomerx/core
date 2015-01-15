bf978c05029726869c7aad8b987c264ed50d3b87
EK <ek@neomerx.com>
2015-01-15 20:35:20 +0300

    Initial commit

---
+++ res/lang/en/application.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'order_status_new' => 'New Order',
+
+    'rules_all' => 'All',
+
+    'product_tax_type_shipping'   => 'Shipping',
+    'product_tax_type_exempt'     => 'Tax Exempt',
+    'product_tax_type_taxable'    => 'Taxable Goods',
+
+    'customer_type_general'       => 'General',
+    'customer_type_member'        => 'Members Only',
+    'customer_type_not_logged_in' => 'Not Logged-in',
+    'customer_type_private'       => 'Private Sales',
+    'customer_type_retail'        => 'Retail Customer',
+    'customer_type_wholesale'     => 'Wholesale Customer',
+
+    'warehouse_default_name'      => 'Default Warehouse',
+
+    'store_default_name'          => 'Acme Widgets',
+];
