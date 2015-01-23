5d30ed9228fba69651525506ecabdcf8b024ee4e
EK <ek@neomerx.com>
2015-01-23 15:06:01 +0300

    Remove unused exceptions

--- lang/en/exceptions.php
+++ lang/en/exceptions.php
@@ -2,12 +2,9 @@
 
 return [
     'access_denied_file_exception' => 'Access to file denied.',
-    'configuration_exception'      => 'Configuration parameter set incorrectly.',
     'exception'                    => 'Unknown error.',
     'file_exception'               => 'Error occurred while working with file.',
     'invalid_argument_exception'   => 'Invalid argument provided.',
     'logic_exception'              => 'Error occurred in program logic.',
-    'not_implemented_exception'    => 'Functionality is not yet implemented.',
     'validation_exception'         => 'Validation failed.',
-    'resource_not_found_exception' => 'Resource not found.',
 ];
