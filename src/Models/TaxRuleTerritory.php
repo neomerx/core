20a4e1b25738d9fd4e5f715ff47c4ed96fc021d6
EK <ek@neomerx.com>
2015-01-25 19:22:39 +0300

    Territory tax rule could be nullable on update. Update model accordingly.

--- src/Models/TaxRuleTerritory.php
+++ src/Models/TaxRuleTerritory.php
@@ -89,7 +89,7 @@ class TaxRuleTerritory extends BaseModel
     {
         return [
             self::FIELD_ID_TAX_RULE  => 'sometimes|required|forbidden',
-            self::FIELD_TERRITORY_ID => 'sometimes|required|integer|min:1|max:4294967295',
+            self::FIELD_TERRITORY_ID => '',
 
             self::FIELD_TERRITORY_TYPE => 'sometimes|required|in:'.
                 self::TERRITORY_TYPE_COUNTRY.','.self::TERRITORY_TYPE_REGION,
