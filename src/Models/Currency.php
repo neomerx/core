afe4207fad33a4aedf5eb46f17b0b52105c949b0
EK <ek@neomerx.com>
2015-07-22 23:36:30 +0300

    Add decimal and thousand separators to currency

--- src/Models/Currency.php
+++ src/Models/Currency.php
@@ -23,13 +23,22 @@ class Currency extends BaseModel implements SelectByCodeInterface
     const CODE_MAX_LENGTH = 3;
 
     /** Model field name */
-    const FIELD_ID             = 'id_currency';
+    const FIELD_ID                       = 'id_currency';
     /** Model field name */
-    const FIELD_CODE           = 'code';
+    const FIELD_CODE                     = 'code';
     /** Model field name */
-    const FIELD_DECIMAL_DIGITS = 'decimal_digits';
+    const FIELD_DECIMAL_DIGITS           = 'decimal_digits';
     /** Model field name */
-    const FIELD_PROPERTIES     = 'properties';
+    const FIELD_DECIMAL_POINTS_SEPARATOR = 'decimal_point';
+    /** Model field name */
+    const FIELD_THOUSANDS_SEPARATOR      = 'thousands_separator';
+    /** Model field name */
+    const FIELD_PROPERTIES               = 'properties';
+
+    /** Default value */
+    const DECIMAL_POINTS_SEPARATOR_DEFAULT = '.';
+    /** Default value */
+    const THOUSANDS_SEPARATOR_DEFAULT      = ',';
 
     /**
      * @inheritdoc
@@ -58,6 +67,8 @@ class Currency extends BaseModel implements SelectByCodeInterface
         self::FIELD_ID,
         self::FIELD_CODE,
         self::FIELD_DECIMAL_DIGITS,
+        self::FIELD_DECIMAL_POINTS_SEPARATOR,
+        self::FIELD_THOUSANDS_SEPARATOR,
     ];
 
     /**
@@ -77,7 +88,9 @@ class Currency extends BaseModel implements SelectByCodeInterface
             self::FIELD_CODE => 'required|code|min:'.self::CODE_MIN_LENGTH.
                 '|max:'.self::CODE_MAX_LENGTH.'|unique:'.self::TABLE_NAME,
 
-            self::FIELD_DECIMAL_DIGITS => 'required|integer|min:0|max:4',
+            self::FIELD_DECIMAL_DIGITS           => 'required|integer|min:0|max:4',
+            self::FIELD_DECIMAL_POINTS_SEPARATOR => 'required|string|size:1',
+            self::FIELD_THOUSANDS_SEPARATOR      => 'string|min:0|max:1',
         ];
     }
 
@@ -87,9 +100,11 @@ class Currency extends BaseModel implements SelectByCodeInterface
     public function getDataOnUpdateRules()
     {
         return [
-            self::FIELD_ID             => 'sometimes|required|forbidden',
-            self::FIELD_CODE           => 'sometimes|required|forbidden',
-            self::FIELD_DECIMAL_DIGITS => 'sometimes|required|integer|min:0|max:4',
+            self::FIELD_ID                       => 'sometimes|required|forbidden',
+            self::FIELD_CODE                     => 'sometimes|required|forbidden',
+            self::FIELD_DECIMAL_DIGITS           => 'sometimes|required|integer|min:0|max:4',
+            self::FIELD_DECIMAL_POINTS_SEPARATOR => 'sometimes|required|string|size:1',
+            self::FIELD_THOUSANDS_SEPARATOR      => 'sometimes|string|min:0|max:1',
         ];
     }
 
